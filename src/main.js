cd8a00d972a5f4221e1e0d7ca276db756926bf21
Cosas de Puma <kikefontanlorenzo@gmail.com>
2018-06-01 07:41:56 +0200

    Electron Skeleton

---
+++ src/main.js
@@ -0,0 +1,41 @@
+const electron = require('electron')
+const app = electron.app
+
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    // width: 400, height: 350,
+    width: 600, height: 450,
+    frame: false, resizable: false
+  })
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function() {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('before-quit', () => {
+    mainWindow.removeAllListeners('close');
+    mainWindow.close();
+});
+
+app.on('activate', function() {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
